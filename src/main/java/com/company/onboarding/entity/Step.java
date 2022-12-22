850db1a71376e395eece8219e127d04dc99137e7
kazantsev.ed <kazantsev.ed@outlook.com>
2022-12-22 22:27:21 +0400

    Initial commit

---
+++ src/main/java/com/company/onboarding/entity/Step.java
@@ -0,0 +1,99 @@
+package com.company.onboarding.entity;
+
+import io.jmix.core.FileRef;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "STEP")
+@Entity
+public class Step {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "FILE_", length = 1024)
+    private FileRef file;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @InstanceName
+    @Column(name = "NAME", nullable = false)
+    @NotNull
+    private String name;
+
+    @Column(name = "DURATION", nullable = false)
+    @NotNull
+    private Integer duration;
+
+    @Column(name = "SOME_TEXT")
+    private String someText;
+
+    @Column(name = "SORT_VALUE", nullable = false)
+    @NotNull
+    private Integer sortValue;
+
+    public FileRef getFile() {
+        return file;
+    }
+
+    public void setFile(FileRef file) {
+        this.file = file;
+    }
+
+    public Integer getSortValue() {
+        return sortValue;
+    }
+
+    public void setSortValue(Integer sortValue) {
+        this.sortValue = sortValue;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getSomeText() {
+        return someText;
+    }
+
+    public void setSomeText(String someText) {
+        this.someText = someText;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
